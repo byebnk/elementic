41a17bda6216ef0ee83082c466482686f80f8d71
Bruno Capelão <brunocapelao@mac-bruno.local>
2023-03-15 08:11:36 -0300

    Melhoria no tratamento dos erros.

--- Elementic_Form_Action.php
+++ Elementic_Form_Action.php
@@ -1,105 +1,91 @@
 <?php
 
-    class Elementic_Form_Action extends \ElementorPro\Modules\Forms\Classes\Action_Base {
-        public function get_name() {return 'Mautic';}
+class Elementic_Form_Action extends \ElementorPro\Modules\Forms\Classes\Action_Base {
+    public function get_name() {return 'Mautic';}
 
-        public function get_label() {return __( 'Mautic', 'text-domain' );}
+    public function get_label() {return __( 'Mautic', 'elementic' );}
 
-        /**
-         * register elementor forms
-         * @param type $widget 
-         * @return type
-         */
-        public function register_settings_section( $widget ) {
+    public function register_settings_section( $widget ) {
 
-            $widget->start_controls_section(
-                'section_mautic',
-                [
-                    'label' => __( 'Mautic', 'text-domain' ),
-                    'condition' => [
-                        'submit_actions' => $this->get_name(),
-                    ],
-                ]
-            );
+        $widget->start_controls_section(
+            'section_mautic',
+            [
+                'label' => __( 'Mautic', 'elementic' ),
+                'condition' => [
+                    'submit_actions' => $this->get_name(),
+                ],
+            ]
+        );
 
-            $widget->add_control(
-                'mautic_url',
-                [
-                    'label' => __( 'Mautic Form URL *', 'text-domain' ),
-                    'type' => \Elementor\Controls_Manager::URL,
-                    'placeholder' => 'http://yourmauticurl.com/',
-                    'label_block' => true,
-                    'separator' => 'before',
-                    'description' => __( 'Enter the URL where you have Mautic installed', 'text-domain' ),
-                ]
-            );
+        $widget->add_control(
+            'mautic_url',
+            [
+                'label' => __( 'Mautic Form URL *', 'elementic' ),
+                'type' => \Elementor\Controls_Manager::URL,
+                'placeholder' => 'http://yourmauticurl.com/',
+                'label_block' => true,
+                'separator' => 'before',
+                'description' => __( 'Enter the URL where you have Mautic installed', 'elementic' ),
+            ]
+        );
 
+        $widget->add_control(
+            'mautic_form_id',
+            [
+                'label' => __('Mautic Form ID *', 'elementic'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'placeholder' => '99',
+                'label_block' => true,
+                'separator' => 'before',
+                'description' => __( 'Fill with your form id', 'elementic' ),
+            ]
+        );
 
-            $widget->add_control(
-                'mautic_form_id',
-                [
-                    'label' => __('Mautic Form ID *', 'text-domain'),
-                    'type' => \Elementor\Controls_Manager::TEXT,
-                    'placeholder' => '99',
-                    'label_block' => true,
-                    'separator' => 'before',
-                    'description' => __( 'Fill with your form id', 'text-domain' ),
-                ]
-            );
+        $widget->end_controls_section();
+    }
 
+    public function run( $record, $ajax_handler ) {
+        $settings = $record->get( 'form_settings' );
 
-            $widget->end_controls_section();
+        if ( empty( $settings['mautic_url'] ) ) {
+            return;
         }
 
-
-        public function run( $record, $ajax_handler ) {
-            $settings = $record->get( 'form_settings' );
-
-            //  Make sure that there is a Mautic url
-            if ( empty( $settings['mautic_url'] ) ) {
-                return;
-            }
-
-            //  Make sure that there have a Form ID
-            if ( empty( $settings['mautic_form_id'] ) ) {
-                return;
-            }
-
-
-
-            // Get sumitetd Form data
-            $raw_fields = $record->get( 'fields' );
-
-            // Normalize the Form Data
-            $fields = [
-                'formId' => $settings['mautic_form_id']
-            ];
-            foreach ( $raw_fields as $id => $field ) {
-                $fields[ $id ] = $field['value'];
-            }
-		
-		
-
-            $response = wp_remote_post(rtrim($settings['mautic_url']['url'],"/")."/form/submit?formId=".$settings['mautic_form_id'], [
-                'body' => ["mauticform" => $fields],
-				'headers' => [ 'X-Forwarded-For' => $_SERVER[ "REMOTE_ADDR" ]]
-            ] );
-
-
-            // $message = preg_match('/<div class=\"well text-center\">(.*?)<\/div>/s', $response['body'], $match);
-
-
-
-
-            // echo trim(strip_tags($match[1]));
-
+        if ( empty( $settings['mautic_form_id'] ) ) {
+            return;
         }
 
+        $raw_fields = $record->get( 'fields' );
 
+        $fields = [
+            'formId' => $settings['mautic_form_id']
+        ];
+        foreach ( $raw_fields as $id => $field ) {
+            $fields[ $id ] = $field['value'];
+        }
 
+        $response = wp_remote_post(rtrim($settings['mautic_url']['url'],"/")."/form/submit?formId=".$settings['mautic_form_id'], [
+            'body' => ["mauticform" => $fields],
+            'headers' => [ 'X-Forwarded-For' => get_user_ip_addr() ]
+        ]);
 
-
-        public function on_export( $element ) {
-            return $element;
+        if (is_wp_error($response)) {
+            // Handle the error appropriately
+            return;
         }
     }
+
+    public function on_export( $element ) {
+        return $element;
+    }
+}
+
+function get_user_ip_addr() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        return $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
